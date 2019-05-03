daa34f2efd7b528fd9828cc934317c0ecd69c971
Christian Casey <christiancasey86@gmail.com>
2019-05-03 14:54:35 -0400

    Deleted debugging junk

--- make_images.py
+++ make_images.py
@@ -5,8 +5,6 @@ vidcap = cv2.VideoCapture('videos/s08e03.mkv')
 success,image = vidcap.read()
 count = 0
 while success:
-	# cv2.imwrite('images/got-s01e01_' + "{:0>4d}".format(count) + '.jpg', image)
 	cv2.imwrite('images/' + "{:0>7d}".format(count) + '.jpg', image)
-	# print('Saved: images/' + "{:0>4d}".format(count) + '.jpg')
 	success,image = vidcap.read()
 	count += 1
